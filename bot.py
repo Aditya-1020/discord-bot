ac22d874e4b2a777fc629e47e84fe5a9991d3d78
Aditya <adityareddy400@gmail.com>
2023-09-18 10:28:59 +0530

    working

--- bot.py
+++ bot.py
@@ -1,22 +1,23 @@
 import discord
 import responses
 
-
+# Define your intents
+intents = discord.Intents.default()
+intents.typing = False  # You can customize your intents as needed
 
 async def send_message(message, user_message, is_private):
     try:
-        response = responses.handle_resposne(user_message)
-        await message.channel.send(response) if is_private else await message.channel.send(reseponse)
+        response = responses.handle_response(user_message)
+        await message.author.send(response) if is_private else await message.channel.send(response)
     except Exception as e:
         print(e)
-    
 
 def run_discord_bot():
-    TOKEN = "MTE1MzE3NDk0MzQ4MDE2NDQyMw.GukJ1A.RLsC7HsO7FCGzbTX7sPtoRoXeLIwaqc2p6ucVY"
-    client = discord.Client()
+    TOKEN = "MTE1MzE3NDk0MzQ4MDE2NDQyMw.GwNdVY.uEChisq1dvhFWOxDcIKQ09DQwfnZt_QviGv5AI"
+    client = discord.Client(intents=intents)  # Pass the intents object
     
     @client.event
     async def on_ready():
         print(f"{client.user} is now running")
-    
+
     client.run(TOKEN)
--- responses.py
+++ responses.py
@@ -1,14 +1,16 @@
-import random
-
-def handle_resposne(message) -> str:
+def handle_response(message) -> str:
     p_message = message.lower()
    
     if p_message == "hello":
         return "Hey there"
    
     if p_message == "roll":
-        return str(random.randint(1,6))
+        return str(random.randint(1, 6))
    
     if p_message == "!help":
         return "This is HELP"
-   
+    
+    if "@adi-bot" in p_message:
+        return "SIKE: "
+    
+    return ""  # Return an empty string if no matching response is found
